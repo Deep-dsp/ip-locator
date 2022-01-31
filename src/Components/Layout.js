c6cea142ebd7d21e206d2cca7f61d733c9a4d1ab
Deepkumar Patel <dspguru97@gmail.com>
2022-01-30 23:53:53 -0500

    Title

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 const Layout = () => {
   return (
-      <div className="content-container">
-         Layout
+      <div className="content-container d-flex justify-content-center align-items-center">
+
+         <div className="app-title">
+            <h1>IP Address Tracker</h1>
+         </div>
+         
       </div>
    );
 };
