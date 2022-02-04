2a1287794a78d9b423dc6ab38f19250e9d437762
Deepkumar Patel <dspguru97@gmail.com>
2022-02-03 23:47:34 -0500

    API Call worked

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -3,20 +3,23 @@ import axios from 'axios';
 
 const Layout = () => {
 
+   // const currentIP = fetch("https://api.ipify.org/?format=json").then( results => results.json()).then( data => console.log(data.ip));
+
    const [ enteredValue, setEnteredvalue ] = useState("");
    const [ searchResults, setSearchResults ] = useState("");
 
    const onSubmitForm = async (e) => {
       e.preventDefault();
       
-      const data = await axios.get("https://geo.ipify.org/api/v2/country", {
+      const data = await axios.get("https://geo.ipify.org/api/v1/", {
          params:{
             apiKey: 'at_POgsJyOItI1DQF68WTu4A9zROtWQw',
             ipAddress: enteredValue
          }
       });
 
-      console.log(data);
+      setSearchResults(data.data);
+      console.log(data.data);
    }
 
 
@@ -44,22 +47,22 @@ const Layout = () => {
 
                <div className="col-md-3">
                   <p className="title">IP ADDRESS</p>
-                  <p className="search-result">125.50.21</p>
+                  <p className="search-result">{ searchResults ? searchResults.ip : "IP"}</p>
                </div>
 
                <div className="col-md-3">
-                  <p className="title">IP ADDRESS</p>
-                  <p className="search-result">125.50.21</p>
+                  <p className="title">LOCATION</p>
+                  <p className="search-result">{ searchResults ? searchResults.location.city + "," + searchResults.location.country + "," + searchResults.location.postalCode : "location"}</p>
                </div>
 
                <div className="col-md-3">
-                  <p className="title">IP ADDRESS</p>
-                  <p className="search-result">125.50.21</p>
+                  <p className="title">TIMEZONE</p>
+                  <p className="search-result">{ searchResults ? searchResults.location.timezone : "timezone" }</p>
                </div>
 
                <div className="col-md-3">
-                  <p className="title">IP ADDRESS</p>
-                  <p className="search-result">125.50.21</p>
+                  <p className="title">ISP</p>
+                  <p className="search-result">{ searchResults ? searchResults.isp : "isp" }</p>
                </div>
 
             </div>
